483f2d6cdb60daf35a6e076949f48dcff8fe1d3d
andrei.cruz <andrei_cruz97@hotmail.com>
2019-12-04 10:48:56 -0300

    map

--- For-forin/map.js
+++ For-forin/map.js
@@ -3,7 +3,6 @@ const service = require('./service')
 async function main(){
     try {
         const results = await service.obterPessoas(`a`)
-        // const names = []
         // results.results.forEach(function(item){
         //     names.push(item.name)
         // })

fc704a8cf8368b8c8dc3e5ff6dc0307208cd263e
andrei.cruz <andrei_cruz97@hotmail.com>
2019-12-05 16:04:11 -0300

    Novos métodos
    Adicionado novos métodos na database

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -7,13 +7,23 @@ const DEFAULT_ITEM_CADASTRADO = {
 }
 
 describe('Suiet de manipulação de heróis', () => {
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRADO)
+    })
     it('deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRADO
         const [resultado] = await database.listar(expected.id)
         deepEqual(resultado, expected)
     })
-    // it('deve cadastrar um heroi, usando arquivos', async () => {
-    //     const expected = DEFAULT_ITEM_CADASTRADO
-    //     ok(null, expected)
-    // })
+    it('deve cadastrar um heroi, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRADO
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRADO)
+        const [atual] = await database.listar(DEFAULT_ITEM_CADASTRADO.id)
+        deepEqual(atual, expected)
+    })
+    it('deve remover um heroi pelo id', async () => {
+        const expected = true
+        const resultado = await database.removerPeloId(DEFAULT_ITEM_CADASTRADO.id)
+        deepEqual(resultado, expected)
+    })
 })
